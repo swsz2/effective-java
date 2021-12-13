fd8d826c1395beda907911008efe8c910764431b
swsz2 <swsz_@naver.com>
2021-12-13 22:07:59 +0900

    Test promotion

---
+++ src/test/java/me/swsz2/effectivejava/chapter2/act2/PromotionTest.java
@@ -0,0 +1,22 @@
+package me.swsz2.effectivejava.chapter2.act2;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author : swsz2
+ * @mailto : swsz@kakao.com
+ * @created : 2021-12-13, 월요일
+ */
+class PromotionTest {
+
+  @Test
+  public void testPromotion() {
+
+    Promotion constructor = new Promotion(true, false, true, true);
+    Promotion builder = new Promotion.Builder().student().ministry().recommender().build();
+
+    System.out.println("constructor = " + constructor);
+    System.out.println("builder = " + builder);
+
+  }
+}
