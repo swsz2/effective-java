d982c2673b03a9d538482210981439e8fb2cb792
swsz2 <swsz_@naver.com>
2021-12-15 22:02:01 +0900

    Create StringUtils

---
+++ src/main/java/me/swsz2/effectivejava/chapter2/act4/StringUtils.java
@@ -0,0 +1,12 @@
+package me.swsz2.effectivejava.chapter2.act4;
+
+/**
+ * @author : swsz2
+ * @mailto : swsz@kakao.com
+ * @created : 2021-12-15, 수요일
+ */
+public class StringUtils {
+  private StringUtils() {
+    throw new AssertionError("Restricted Class");
+  }
+}
